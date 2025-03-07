333eb21b19b9d72e080ed3afd59acc2e2e9f0ba4
iancampusano <34844163+iancampusano@users.noreply.github.com>
2025-03-07 13:52:29 -0300

    se agregan filtros

--- src/controllers/gameController.js
+++ src/controllers/gameController.js
@@ -2,22 +2,63 @@ const GameModel = require('../models/gameModel');
 
 exports.getGames = async (req, res, next) => {
     try {
-        const { name } = req.query; // Obtiene el parámetro de consulta "name"
+        let { name, platforms, edition, service, sort } = req.query;
         let games = await GameModel.getAllGames();
 
         if (!games || games.length === 0) {
             return res.status(404).json({ message: "No hay juegos disponibles en la base de datos" });
         }
 
-        // Filtrar juegos si se proporciona un nombre
+        // Filtrar juegos por nombre
         if (name) {
             games = games.filter(game =>
                 game.play_nombre.toLowerCase().includes(name.toLowerCase())
             );
+        }
 
-            if (games.length === 0) {
-                return res.status(404).json({ message: `No se encontraron juegos con el nombre "${name}"` });
+        // Filtrar por plataforma
+        if (platforms) {
+            games = games.filter(game =>
+                game.play_platforms.toLowerCase() === platforms.toLowerCase()
+            );
+        }
+
+        // Filtrar por edición
+        if (edition) {
+            games = games.filter(game =>
+                game.play_edition.toLowerCase() === edition.toLowerCase()
+            );
+        }
+
+        // Filtrar por servicio adicional
+        if (service) {
+            games = games.filter(game =>
+                game.play_additional_service.toLowerCase() === service.toLowerCase()
+            );
+        }
+
+        // Ordenamiento
+        if (sort) {
+            switch (sort) {
+                case "name":
+                    games.sort((a, b) => a.play_nombre.localeCompare(b.play_nombre));
+                    break;
+                case "original_price":
+                    games.sort((a, b) => a.play_original_price - b.play_original_price);
+                    break;
+                case "current_price":
+                    games.sort((a, b) => a.play_current_price - b.play_current_price);
+                    break;
+                case "discount":
+                    games.sort((a, b) => b.play_discount - a.play_discount);
+                    break;
+                default:
+                    break;
             }
+        }
+
+        if (games.length === 0) {
+            return res.status(404).json({ message: "No se encontraron juegos con los filtros aplicados" });
         }
 
         res.status(200).json({
