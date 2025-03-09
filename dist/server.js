53962588d5bbdfa97ecb579ba2d6411595657158
iancampusano <34844163+iancampusano@users.noreply.github.com>
2025-03-09 17:00:53 -0300

    deploy

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,22 @@
-require('dotenv').config(); // Asegura que las variables de entorno se carguen
+require("dotenv").config();
+const express = require("express");
+const serverless = require("serverless-http");
 
-const app = require('./src/app');
-const PORT = process.env.PORT || 4000;
+const app = express();
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+// Rutas
+const gameRoutes = require("./routes/gameRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+// Definir endpoints
+app.use("/api/games", gameRoutes);
+app.use("/api/auth", authRoutes);
+
+// Middleware para manejar rutas no encontradas
+app.use((req, res) => {
+    res.status(404).json({ error: "Ruta no encontrada" });
 });
+
+// Exportar el handler de Express para AWS Lambda
+module.exports.handler = serverless(app);
